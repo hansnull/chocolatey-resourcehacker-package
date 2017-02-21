999ed7060829516aff5c4eb4a9f22e274dc41c02
hansnull <t-len@gmx.de>
2017-02-21 10:51:07 +0100

    Resource Hacker 4.5.30 initial version.

---
+++ resourcehacker/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'resourcehacker'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www.angusj.com/resourcehacker/reshacker_setup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'Resource Hacker*'
+
+  checksum      = '78d293bea40e7e159728eecfa86ec085'
+  checksumType  = 'md5'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
